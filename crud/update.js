c8ebc44ec7c5415c633e4e9f488ad61f8aed4f1a
christian <chmartin22@yahoo.es>
2021-01-12 13:01:58 +0100

    Peleando con la vida
    y el update

--- crud/update.js
+++ crud/update.js
@@ -3,13 +3,19 @@ const libro = require ("../models/libro");
 const mongoose = require("mongoose"); 
 
 const updateLibro = async (titulo, campoCambio, nuevoValor) => {
-    console.log(campoCambio);
-    const nuevoLibro = await libro.findOneAndUpdate ({title: titulo}, {campoCambio: nuevoValor}, {new: true});
+    
+    // const nuevoLibro = await libro.findOneAndUpdate ({title: titulo}, {cambio: nuevoValor}, {new: true});
+    // console.log (nuevoLibro);
+    let libroUpdate = new libro();
+    libroUpdate.author = campoCambio;
+    const nuevoLibro = await libro.findOneAndUpdate ({title: titulo}, {libroUpdate.author : nuevoValor}, {new: true})
     console.log (nuevoLibro);
+
+    
     mongoose.connection.close(console.log("cerrado")); 
 };
 
-updateLibro ("El Quijote","author", "Cervantes JrJr");  
+updateLibro ("Quijote","author", "Cervantes JrJr");  
 
 //Meter en la llamada a la función el campo a cambiar.
 //Importar funciones?
